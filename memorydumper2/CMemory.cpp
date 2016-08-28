10e4519bf60e4f71d439a57bdd8f8369595f02f1
Mike Ash <mike@mikeash.com>
2016-08-27 22:02:49 -0400

    Dump a simple C++ subclass and a multiply-inheriting C++ subclass.

--- memorydumper2/CMemory.cpp
+++ memorydumper2/CMemory.cpp
@@ -4,16 +4,17 @@
 
 void DumpCMemory(void (^dump)(const void *ptr, size_t knownSize, long maxDepth, const char *name)) {
     struct S {
-        int x;
-        int y;
-        int z;
+        long x;
+        long y;
+        long z;
     };
     
     S s = { 1, 2, 3 };
     dump(&s, sizeof(s), 10, "Simple C struct");
     
     class SimpleClass {
-        int x;
+    public:
+        long x;
         
         virtual void f() {}
         virtual void g() {}
@@ -21,5 +22,37 @@ void DumpCMemory(void (^dump)(const void *ptr, size_t knownSize, long maxDepth, 
     };
     
     SimpleClass simpleClass;
+    simpleClass.x = 1;
     dump(&simpleClass, sizeof(simpleClass), 10, "Simple C++ class");
+    
+    class SimpleSubclass: public SimpleClass {
+    public:
+        long y;
+        
+        virtual void i() {}
+        virtual void j() {}
+    };
+    
+    SimpleSubclass simpleSubclass;
+    simpleSubclass.x = 1;
+    simpleSubclass.y = 2;
+    dump(&simpleSubclass, sizeof(simpleSubclass), 10, "Simple C++ subclass");
+    
+    class SecondSuperclass {
+    public:
+        long z;
+        
+        virtual void k() {}
+        virtual void l() {}
+    };
+    
+    class MultipleInheritanceSubclass: public SimpleClass, SecondSuperclass {
+    public:
+        long a;
+    };
+    
+    MultipleInheritanceSubclass multipleInheritanceSubclass;
+    multipleInheritanceSubclass.x = 1;
+    multipleInheritanceSubclass.a = 2;
+    dump(&multipleInheritanceSubclass, sizeof(multipleInheritanceSubclass), 10, "C++ subclass with two superclasses");
 }
